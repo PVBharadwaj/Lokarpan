4d9d2cc0662eacfc53374004326340f677bf2921
Arpit Batra <arpitbatra123@gmail.com>
2018-09-08 22:55:26 +0530

    Fix Search

--- static/js/search-users.js
+++ static/js/search-users.js
@@ -1,20 +1,16 @@
-// Search Matches need to be arranged properly
-// Edit Styling to start form center
-
-
 function myFunction() {
 
     const users = document.getElementsByClassName("profile-name");
     const titles = document.getElementsByClassName("profile-title");
-    // const profile = document.getElementsByClassName("profile-title");
+    const profiles = document.getElementsByClassName("profile-text");
     let searchquery = document.getElementsByClassName('form-control')[0].value;
     searchquery = searchquery.toLocaleUpperCase();
 
     // console.log(searchquery)
 
     for (let i = 0; i < users.length; i++) {
-        //console.log(titles[i].innerText.toLocaleUpperCase());
-        if (users[i].innerText.toLocaleUpperCase().indexOf(searchquery) > -1 || titles[i].innerText.toLocaleUpperCase().indexOf(searchquery) > -1) {
+        console.log(profiles[i].innerText.toLocaleUpperCase());
+        if (users[i].innerText.toLocaleUpperCase().indexOf(searchquery) > -1 || titles[i].innerText.toLocaleUpperCase().indexOf(searchquery) > -1 || profiles[i].innerText.toLocaleUpperCase().indexOf(searchquery) > -1) {
             users[i].parentElement.parentElement.parentElement.style.display = "";
         } else {
             users[i].parentElement.parentElement.parentElement.style.display = "none";
